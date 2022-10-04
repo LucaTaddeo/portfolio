97d7e8e03790e206666b0082213ba02ada82f741
Luca Taddeo <lucataddeo2000@gmail.com>
2022-10-05 00:14:27 +0200

    Test CI/CD [Deploy]

--- src/src/App.js
+++ src/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React BETTER
+          This is a new text!
         </a>
       </header>
     </div>
