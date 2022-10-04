8277480d7a30d6924dbdad0bc9dfc7a42cd85b8d
Luca Taddeo <lucataddeo2000@gmail.com>
2022-10-05 00:39:15 +0200

    Initialize new app with nextUI and useDarkMode [Deploy]

--- src/src/App.js
+++ src/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { createTheme, NextUIProvider, Text } from "@nextui-org/react"
+import useDarkMode from 'use-dark-mode';
+
+const lightTheme = createTheme({
+  type: 'light',
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
 
 function App() {
+  const darkMode = useDarkMode(true);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is a new text!
-        </a>
-      </header>
-    </div>
+    <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh"}} >
+      <Text
+        h1
+        size={60}
+        css={{
+          textGradient: "45deg, $blue600 -20%, $pink600 50%",
+        }}
+        weight="bold"
+      >
+        Website
+      </Text>
+      <Text
+        h1
+        size={60}
+        css={{
+          textGradient: "45deg, $purple600 -20%, $pink600 100%",
+        }}
+        weight="bold"
+      >
+        Under
+      </Text>
+      <Text
+        h1
+        size={60}
+        css={{
+          textGradient: "45deg, $yellow600 -20%, $red600 100%",
+        }}
+        weight="bold"
+      >
+        Construction
+      </Text>
+      </div>
+    </NextUIProvider>
   );
 }
 
