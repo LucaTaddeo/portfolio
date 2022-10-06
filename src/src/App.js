fc00beea19a822bdfb17a0148346803849491cae
Luca Taddeo <lucataddeo2000@gmail.com>
2022-10-06 12:45:07 +0200

    Initialize React Router and Home

--- src/src/App.js
+++ src/src/App.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import { createTheme, NextUIProvider, Text } from "@nextui-org/react"
+import { createTheme, NextUIProvider } from "@nextui-org/react"
 import useDarkMode from 'use-dark-mode';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
 
 const lightTheme = createTheme({
   type: 'light',
@@ -10,43 +16,20 @@ const darkTheme = createTheme({
   type: 'dark',
 })
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: []
+  }
+]);
+
 function App() {
   const darkMode = useDarkMode(true);
   
   return (
     <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
-      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh"}} >
-      <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "45deg, $blue600 -20%, $pink600 50%",
-        }}
-        weight="bold"
-      >
-        Website
-      </Text>
-      <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "45deg, $purple600 -20%, $pink600 100%",
-        }}
-        weight="bold"
-      >
-        Under
-      </Text>
-      <Text
-        h1
-        size={60}
-        css={{
-          textGradient: "45deg, $yellow600 -20%, $red600 100%",
-        }}
-        weight="bold"
-      >
-        Construction
-      </Text>
-      </div>
+        <RouterProvider router={router} />
     </NextUIProvider>
   );
 }
---
+++ src/src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Text } from "@nextui-org/react";
+import React from "react";
+ 
+const Home = (props) => {
+    return (
+        <>
+            <Text>This is the home</Text>
+        </>
+    )
+}
+ 
+export default Home;
