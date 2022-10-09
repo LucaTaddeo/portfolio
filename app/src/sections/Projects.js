27df2a7582ee91ec6a2e76effb442d53a5a80583
Luca Taddeo <lucataddeo2000@gmail.com>
2022-10-09 23:39:08 +0200

    Begin creation of Projects section

---
+++ app/src/sections/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from "@nextui-org/react";
+import SectionContainer from "../components/SectionContainer";
+
+const Projects = () => {
+
+    return <SectionContainer title="Projects">
+        <Text>These are some of the projects I worked on in the last years!</Text>
+    </SectionContainer>;
+}
+
+export default Projects;
