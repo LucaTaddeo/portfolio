bc88dc570dac060ae107d60724b6dccdbb853576
Luca Taddeo <lucataddeo2000@gmail.com>
2022-10-09 13:37:05 +0200

    Add Linkedin contact

--- app/src/data/contacts.js
+++ app/src/data/contacts.js
@@ -1,29 +1,38 @@
 import InstagramIcon from "@mui/icons-material/Instagram";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import USER from "./thisIsMe";
 
-const defaultSubjectForEmails = "Hello from lucataddeo.com!";
+export const SUBJECT_FOR_EMAILS = "Hello from lucataddeo.com!";
 
 const CONTACTS = [
-    {
-        name: "Instagram",
-        icon: <InstagramIcon style={{margin: 0}}/>, 
-        link: "https://instagram.com/lucataddeo",
-        action: () => window.open("https://instagram.com/lucataddeo")
-    },
-    {
-        name: "Github",
-        icon: <GitHubIcon style={{margin: 0}}/>,
-        link: "https://github.com/LucaTaddeo",
-        action: () => window.open("https://github.com/LucaTaddeo")
-    },
-    {
-        name: "E-mail",
-        icon: <MailOutlineIcon style={{margin: 0}}/>,
-        link: "mailto:"+USER.email,
-        action: () => window.location.href = "mailto:"+USER.email+"?subject="+defaultSubjectForEmails,
-    }
+  {
+    name: "Github",
+    icon: <GitHubIcon style={{ margin: 0 }} />,
+    link: "https://github.com/LucaTaddeo",
+    action: () => window.open("https://github.com/LucaTaddeo"),
+  },
+  {
+    name: "Linkedin",
+    icon: <LinkedInIcon style={{ margin: 0 }} />,
+    link: "https://www.linkedin.com/in/lucataddeo",
+    action: () => window.open("https://www.linkedin.com/in/lucataddeo"),
+  },
+  {
+    name: "Instagram",
+    icon: <InstagramIcon style={{ margin: 0 }} />,
+    link: "https://instagram.com/lucataddeo",
+    action: () => window.open("https://instagram.com/lucataddeo"),
+  },
+  {
+    name: "E-mail",
+    icon: <MailOutlineIcon style={{ margin: 0 }} />,
+    link: "mailto:" + USER.email,
+    action: () =>
+      (window.location.href =
+        "mailto:" + USER.email + "?subject=" + SUBJECT_FOR_EMAILS),
+  },
 ];
 
 export default CONTACTS;
